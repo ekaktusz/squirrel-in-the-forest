63d61b0b1e5e9304eea0cd81043094a57e463b12
Balazs <balzs.gyngysi@gmail.com>
2024-11-07 21:48:13 +0100

    added placeholder tileset, added trees and hide mechanic base, added teleport to start on enemy hit

--- Scenes/NPC/npc.gd
+++ Scenes/NPC/npc.gd
@@ -1,28 +1,31 @@
 extends CharacterBody2D
 
 @onready var npc: CharacterBody2D = $"."
-const SPEED = 30
-const ACCELERATION = 1
+@export var style : int
+const SPEED = 60
+const ACCELERATION = 2
 var direction: Vector2 = Vector2.UP
 var direction_2: Vector2 = Vector2.LEFT
-	
-func move():
-	velocity = direction * SPEED * ACCELERATION
-	move_and_slide()
-	if is_on_ceiling() or is_on_floor():
-		change_direction()
+		
+func _process(_delta):
+	if (style == Globals.NpcType.UP_DOWN):
+		("shpuld move updown")
+		velocity = direction * SPEED * ACCELERATION
+		move_and_slide()
+		if is_on_ceiling() or is_on_floor():
+			change_direction()
+	elif (style == Globals.NpcType.SIDE_TO_SIDE):
+		("shpuld move side")
+		velocity = direction_2 * SPEED * ACCELERATION
+		move_and_slide()
+		if is_on_wall():
+			change_direction_side()
 		
 func change_direction():
 	if direction == Vector2.UP:
 		direction = Vector2.DOWN
 	else: 
 		direction = Vector2.UP
-
-func move_side():
-	velocity = direction_2 * SPEED * ACCELERATION
-	move_and_slide()
-	if is_on_wall():
-		change_direction_side()
 
 func change_direction_side():
 	if direction_2 == Vector2.LEFT:
--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -1,10 +1,16 @@
 extends Node
 
+enum NpcType { STATIC, UP_DOWN, SIDE_TO_SIDE, FOLLOW }
 
 var nut_counter: int = 0
-const speed = 50
-const acceleration = 25
-const friction = 10
+
+const speed = 150
+const acceleration = 50
+const friction = 20
+
+#to do add state manager later if need
+var hiding = false
+var able_to_hide = false
 
 func collect_nut():
 	nut_counter += 1
