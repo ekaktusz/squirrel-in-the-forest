d876bbd4f5089186659c4e1b84a4ff7cbe509f62
Balazs <balzs.gyngysi@gmail.com>
2024-11-13 16:29:42 +0100

    anim add and tileset resize

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -4,10 +4,11 @@ enum NpcType { STATIC, UP_DOWN, SIDE_TO_SIDE, FOLLOW }
 
 var total_nut_counter: int = 0
 var level_nut_counter: int = 0
-var ready_to_evacuate: bool = false
+var changing_level: bool = false
+
 var speed = 150
 const acceleration = 50
-const friction = 20
+const friction = 30
 
 var power_up_speed_available = true
 var power_up_invis_available = false
@@ -15,6 +16,8 @@ var power_up_invis_available = false
 #to do add state manager later if need
 var hiding = false
 var able_to_hide = false
+var ready_to_evacuate: bool = false
+var in_start_area: bool = false
 
 func collect_nut():
 	total_nut_counter += 1
@@ -23,4 +26,5 @@ func collect_nut():
 		ready_to_evacuate = true
 
 func lose_nut():
-	level_nut_counter -= 1
+	if level_nut_counter >1:
+		level_nut_counter -= 1
