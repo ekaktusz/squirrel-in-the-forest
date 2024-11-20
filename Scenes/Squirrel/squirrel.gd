15f80b1e5fb4445a3766db2add7d9eb5a4aefdff
Csaba Ekart <csaba.ekart@gmail.com>
2024-11-20 15:48:09 +0100

    project refactor part 1

--- Scenes/Squirrel/squirrel.gd
+++ Scenes/Squirrel/squirrel.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
-@onready var squirrel: CharacterBody2D = $"."
+
+@onready var squirrel: CharacterBody2D = self
 @onready var sprite_collision: CollisionShape2D = $SpriteCollision
 @onready var area_collision: CollisionShape2D = $Area2D/AreaCollision
 @onready var power_up_speed_timer: Timer = $PowerUpSpeedTimer
@@ -9,94 +10,106 @@ extends CharacterBody2D
 @onready var enter_timer: Timer = $EnterTimer
 @onready var area_2d: Area2D = $Area2D
 
-var exit_enter_in_progress = true
-
-var state: Globals.SquirrelState = Globals.SquirrelState.NORMAL
-
 signal enemy_hit
 signal level_done
+
+var state: Globals.SquirrelState = Globals.SquirrelState.NORMAL
+var exit_enter_in_progress = true
+
+const COLLISION_LAYER_ENEMY = 3
+const COLLISION_LAYER_PLAYER = 2
 
 func _ready() -> void:
 	await get_tree().create_timer(2.2).timeout
 	exit_enter_in_progress = false
 	
-
 func _input(event):
+	if exit_enter_in_progress:
+		return
+		
+	if state != Globals.SquirrelState.NORMAL:
+		return
 	
-	#POWER UP HANDLE
+	var can_power_up_speed_pick_up: bool = event.is_action_pressed("power_up_speed") && Globals.power_up_speed_available
 	
-	#SPEEDY
-	if (!exit_enter_in_progress && event.is_action_pressed("power_up_speed") && Globals.power_up_speed_available && state == Globals.SquirrelState.NORMAL):
-		state = Globals.SquirrelState.SPEEDY
-		power_up_speed_timer.start()
-		Globals.power_up_speed_available = false
-		add_power_up_progressbar()
-		Globals.speed = Globals.speed * 2
-	#INVISIBLE
-	if (!exit_enter_in_progress && event.is_action_pressed("power_up_invisible") && Globals.power_up_invis_available && state == Globals.SquirrelState.NORMAL):
-		state = Globals.SquirrelState.INVISIBLE
-		power_up_invisible_timer.start()
-		Globals.power_up_invis_available = false
-		add_power_up_progressbar()
-		area_2d.set_collision_mask_value(3,false)
-		area_2d.set_collision_layer_value(2,false)
-		squirrel.set_collision_mask_value(3,false)
-		squirrel.set_collision_layer_value(2,false)
-
+	if can_power_up_speed_pick_up:
+		activate_power_up_speed()
+		
+	var can_power_up_invis_pick_up: bool = event.is_action_pressed("power_up_invisible") && Globals.power_up_invis_available
+	
+	if can_power_up_invis_pick_up:
+		activate_power_up_invisible()
+		
+func activate_power_up_speed():
+	state = Globals.SquirrelState.SPEEDY
+	power_up_speed_timer.start()
+	Globals.power_up_speed_available = false
+	add_power_up_progressbar()
+	Globals.speed = Globals.speed * 2
+	
+func activate_power_up_invisible():
+	state = Globals.SquirrelState.INVISIBLE
+	power_up_invisible_timer.start()
+	Globals.power_up_invis_available = false
+	add_power_up_progressbar()
+	area_2d.set_collision_mask_value(COLLISION_LAYER_ENEMY, false)
+	area_2d.set_collision_layer_value(COLLISION_LAYER_PLAYER, false)
+	squirrel.set_collision_mask_value(COLLISION_LAYER_ENEMY, false)
+	squirrel.set_collision_layer_value(COLLISION_LAYER_PLAYER, false)
+	
 func add_power_up_progressbar():
 	progress_bar.visible = true
 	progress_bar.value = 100
 	var tween = get_tree().create_tween()
-	tween.tween_property(progress_bar,"value",0,2.0)		
+	tween.tween_property(progress_bar,"value",0,2.0)	
 		
 func _physics_process(_delta: float) -> void:
+	if exit_enter_in_progress:
+		return
 	
-	#todo maybe good idea to add state manager if a lot of states come up
-	if (!exit_enter_in_progress):
+	handle_hiding()
+	handle_evacuation()
+	handle_movement()
 		
-		#HIDE
-		if (Globals.hiding):
-			sprite_collision.disabled = true
-			area_collision.disabled = true
-			squirrel.visible = false
-		else: 
-			sprite_collision.disabled = false
-			area_collision.disabled = false
-			squirrel.visible = true
-		if (Input.is_action_just_pressed("hide") && Globals.able_to_hide):
-			print("hiding")
-			Globals.hiding = true
-		if (Input.is_action_just_released("hide")):
-			print("stopped hiding")
-			Globals.hiding = false
+func handle_hiding() -> void:
+	sprite_collision.disabled = Globals.hiding
+	area_collision.disabled = Globals.hiding
+	squirrel.visible = not Globals.hiding
+	
+	if Input.is_action_just_pressed("hide") and Globals.able_to_hide:
+		print("hiding")
+		Globals.hiding = true
+	if Input.is_action_just_released("hide"):
+		print("stopped hiding")
+		Globals.hiding = false
 		
-		#EVACUATE	
-		if (Input.is_action_just_pressed("evacuate") && Globals.ready_to_evacuate && Globals.in_start_area):
-			exit_enter_in_progress = true
-			animated_sprite_2d.play("evacuate")
-			sprite_collision.disabled = true
-			area_collision.disabled = true
-			await get_tree().create_timer(2.2).timeout
-			level_done.emit()
-			
-		#MOVEMENT
-		var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-		if direction:
-			play_movement_anim(direction, state)
+func handle_evacuation() -> void:
+	if Input.is_action_just_pressed("evacuate") and Globals.ready_to_evacuate and Globals.in_start_area:
+		exit_enter_in_progress = true
+		animated_sprite_2d.play("evacuate")
+		sprite_collision.disabled = true
+		area_collision.disabled = true
+		await get_tree().create_timer(2.2).timeout
+		level_done.emit()
+		
+func handle_movement() -> void:
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		play_movement_anim(direction, state)
 
-			velocity = velocity.move_toward(direction * Globals.speed, Globals.acceleration)
-		else:
-			velocity = velocity.move_toward(Vector2.ZERO, Globals.friction)
-			if (!exit_enter_in_progress):
-				match state:
-					Globals.SquirrelState.NORMAL:
-						animated_sprite_2d.play("idle")
-					Globals.SquirrelState.SPEEDY:
-						animated_sprite_2d.play("idle_speedy")
-					Globals.SquirrelState.INVISIBLE:
-						animated_sprite_2d.play("idle_invis")
-		if (!Globals.hiding):
-			move_and_slide()
+		velocity = velocity.move_toward(direction * Globals.speed, Globals.acceleration)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, Globals.friction)
+		if (!exit_enter_in_progress):
+			match state:
+				Globals.SquirrelState.NORMAL:
+					animated_sprite_2d.play("idle")
+				Globals.SquirrelState.SPEEDY:
+					animated_sprite_2d.play("idle_speedy")
+				Globals.SquirrelState.INVISIBLE:
+					animated_sprite_2d.play("idle_invis")
+	if (!Globals.hiding):
+		move_and_slide()
 
 func play_movement_anim(direction: Vector2, state: Globals.SquirrelState):
 	match state:
