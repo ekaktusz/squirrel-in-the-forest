63aff6eefbf013881deded5e3cb4c9852c46b788
Csaba Ekart <csaba.ekart@gmail.com>
2024-11-23 20:34:28 +0100

    further improvement to ease level addition

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -1,15 +1,12 @@
 extends Node2D
 
-const DUMMY_LEVEL = preload("res://Scenes/Levels/DummyLevel.tscn")
-const LEVEL_1 = preload("res://Scenes/Levels/Level_1.tscn")
+const levels = {
+	0: preload("res://Scenes/Levels/level_0.tscn"),
+	1: preload("res://Scenes/Levels/level_1.tscn")
+}
+
 @onready var hud: Node2D = $CanvasLayer/Hud
 @onready var level_changer: Control = $LevelChanger
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -19,13 +16,11 @@ func _process(_delta: float) -> void:
 
 func _on_level_changer_change_level(level) -> void:
 	reset_level_logic()
-	match level:
-		0:
-			var	dummy_level = DUMMY_LEVEL.instantiate()
-			get_parent().add_child(dummy_level)
-		1:
-			var	level_1 = LEVEL_1.instantiate()
-			get_parent().add_child(level_1)
+	if levels.has(level): 
+		var new_level = levels[level].instantiate()
+		get_parent().add_child(new_level)
+	else:
+		print("Level not found: " + str(level))
 
 func reset_level_logic():
 	hud.visible = true
