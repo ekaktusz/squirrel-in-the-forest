06dc2855bb68f1d72e0d704037767e6e033d8d45
Csaba Ekart <csaba.ekart@gmail.com>
2024-11-24 19:30:47 +0100

    yes

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -8,19 +8,11 @@ const levels = {
 @onready var hud: Node2D = $CanvasLayer/Hud
 @onready var level_changer: Control = $LevelChanger
 
-@onready var s_material: ShaderMaterial = $Background.material as ShaderMaterial
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Globals.changing_level:
 		level_changer.show()
-	# Get mouse position in the viewport
-	var mouse_pos = get_viewport().get_mouse_position()
-	var viewport_size = get_viewport().size
-	# Normalize mouse position to a range of 0.0 to 1.0
-	var normalized_mouse_pos = Vector2(mouse_pos) / Vector2(viewport_size)
-	# Pass normalized mouse position to the shader
-	s_material.set_shader_parameter("mouse_position", normalized_mouse_pos)
 
 
 func _on_level_changer_change_level(level) -> void:
