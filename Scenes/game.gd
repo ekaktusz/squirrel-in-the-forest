79499abe02b7398f85f63cabeff242f1fc8f0e74
danigeri <danigerci@gmail.com>
2024-11-27 22:04:28 +0100

    Buttons are added to game screen.

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -2,7 +2,12 @@ extends Node2D
 
 const DUMMY_LEVEL = preload("res://Scenes/Levels/DummyLevel.tscn")
 const LEVEL_1 = preload("res://Scenes/Levels/Level_1.tscn")
+const LEVEL_2 = preload("res://Scenes/Levels/Level_2.tscn")
+const LEVEL_3 = preload("res://Scenes/Levels/Level_3.tscn")
 const LEVEL_4 = preload("res://Scenes/Levels/Level_4.tscn")
+const LEVEL_5 = preload("res://Scenes/Levels/Level_5.tscn")
+const LEVEL_6 = preload("res://Scenes/Levels/Level_6.tscn")
+const LEVEL_7 = preload("res://Scenes/Levels/Level_7.tscn")
 
 @onready var hud: Node2D = $CanvasLayer/Hud
 @onready var level_changer: Control = $LevelChanger
@@ -26,11 +31,19 @@ func _on_level_changer_change_level(level) -> void:
 			var	dummy_level = DUMMY_LEVEL.instantiate()
 			get_parent().add_child(dummy_level)
 		1:
-			var	level_1 = LEVEL_1.instantiate()
-			get_parent().add_child(level_1)
-			
+			get_parent().add_child(LEVEL_1.instantiate())
+		2:
+			get_parent().add_child(LEVEL_2.instantiate())
+		3:
+			get_parent().add_child(LEVEL_3.instantiate())
 		4:
 			get_parent().add_child(LEVEL_4.instantiate())
+		5:
+			get_parent().add_child(LEVEL_5.instantiate())
+		6:
+			get_parent().add_child(LEVEL_6.instantiate())
+		7:
+			get_parent().add_child(LEVEL_7.instantiate())
 
 func reset_level_logic():
 	hud.visible = true
