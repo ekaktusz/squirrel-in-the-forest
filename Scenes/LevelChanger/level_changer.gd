6a3e4f23e2e64d343a13848f9aaba1efce6203b5
danigeri <danigerci@gmail.com>
2024-11-30 11:33:36 +0100

    Levels can be selected from game screen.

--- Scenes/LevelChanger/level_changer.gd
+++ Scenes/LevelChanger/level_changer.gd
@@ -37,3 +37,33 @@ func _on_level_3_pressed() -> void:
 
 func _on_level_2_pressed() -> void:
 	change_level.emit(2)
+
+
+func _on_level_13_pressed() -> void:
+	change_level.emit(13)
+	pass # Replace with function body.
+
+
+func _on_level_12_pressed() -> void:
+	change_level.emit(12)
+	pass # Replace with function body.
+
+
+func _on_level_11_pressed() -> void:
+	change_level.emit(11)
+	pass # Replace with function body.
+
+
+func _on_level_10_pressed() -> void:
+	change_level.emit(10)
+	pass # Replace with function body.
+
+
+func _on_level_9_pressed() -> void:
+	change_level.emit(9)
+	pass # Replace with function body.
+
+
+func _on_level_8_pressed() -> void:
+	change_level.emit(8)
+	pass # Replace with function body.
--- Scenes/game.gd
+++ Scenes/game.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
 const DUMMY_LEVEL = preload("res://Scenes/Levels/DummyLevel.tscn")
-const LEVEL_1 = preload("res://Scenes/Levels/Level_1.tscn")
-const LEVEL_2 = preload("res://Scenes/Levels/Level_2.tscn")
-const LEVEL_3 = preload("res://Scenes/Levels/Level_3.tscn")
-const LEVEL_4 = preload("res://Scenes/Levels/Level_4.tscn")
-const LEVEL_5 = preload("res://Scenes/Levels/Level_5.tscn")
-const LEVEL_6 = preload("res://Scenes/Levels/Level_6.tscn")
-const LEVEL_7 = preload("res://Scenes/Levels/Level_7.tscn")
+const LEVEL_1 = preload("res://Scenes/Levels/level_1.tscn")
+const LEVEL_2 = preload("res://Scenes/Levels/level_2.tscn")
+const LEVEL_3 = preload("res://Scenes/Levels/level_3.tscn")
+const LEVEL_4 = preload("res://Scenes/Levels/level_4.tscn")
+const LEVEL_5 = preload("res://Scenes/Levels/level_5.tscn")
+const LEVEL_6 = preload("res://Scenes/Levels/level_6.tscn")
+const LEVEL_7 = preload("res://Scenes/Levels/level_7.tscn")
+const LEVEL_8 = preload("res://Scenes/Levels/level_8.tscn")
+const LEVEL_9 = preload("res://Scenes/Levels/level_9.tscn")
+const LEVEL_10 = preload("res://Scenes/Levels/level_10.tscn")
+const LEVEL_11 = preload("res://Scenes/Levels/level_11.tscn")
+const LEVEL_12 = preload("res://Scenes/Levels/level_12.tscn")
+const LEVEL_13 = preload("res://Scenes/Levels/level_13.tscn")
 
 @onready var hud: Node2D = $CanvasLayer/Hud
 @onready var level_changer: Control = $LevelChanger
@@ -44,6 +50,19 @@ func _on_level_changer_change_level(level) -> void:
 			get_parent().add_child(LEVEL_6.instantiate())
 		7:
 			get_parent().add_child(LEVEL_7.instantiate())
+		8:
+			get_parent().add_child(LEVEL_8.instantiate())
+		9:
+			get_parent().add_child(LEVEL_9.instantiate())
+		10:
+			get_parent().add_child(LEVEL_10.instantiate())			
+		11:
+			get_parent().add_child(LEVEL_11.instantiate())			
+		12:
+			get_parent().add_child(LEVEL_12.instantiate())
+		13:
+			get_parent().add_child(LEVEL_13.instantiate())
+
 
 func reset_level_logic():
 	hud.visible = true
