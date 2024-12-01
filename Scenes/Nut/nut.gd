3f3fc4df68be81d8b0c9e919fb5f9e873a93cb61
Csaba Ekart <csaba.ekart@gmail.com>
2024-12-01 20:10:59 +0100

    sam fischer skill

--- Scenes/Nut/nut.gd
+++ Scenes/Nut/nut.gd
@@ -3,6 +3,10 @@ extends Node2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $Sprite2D
 @onready var sfx: AudioStreamPlayer2D = $SFX
 
+func _ready() -> void:
+	if (Globals.selected_power_up == Globals.RelicType.Reveal):
+		z_index = 100
+		animated_sprite_2d.modulate = Color(Color.GREEN)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
