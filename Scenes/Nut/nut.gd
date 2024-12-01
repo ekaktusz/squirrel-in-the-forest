9c5c1abd5b585140d63c195cb5c529094020876e
danigeri <danigerci@gmail.com>
2024-12-01 01:16:07 +0100

    Nut assets are added.

--- Scenes/Nut/nut.gd
+++ Scenes/Nut/nut.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $Sprite2D
+
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		animated_sprite_2d.play("pickup")
+		await  animated_sprite_2d.animation_finished
 		queue_free()
