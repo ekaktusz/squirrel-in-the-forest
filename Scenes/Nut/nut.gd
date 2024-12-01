8349842c39eaf77763d8e9b807a4cb2e0a6529cc
Balazs <balzs.gyngysi@gmail.com>
2024-12-01 18:36:33 +0100

    add sfx and music

--- Scenes/Nut/nut.gd
+++ Scenes/Nut/nut.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $Sprite2D
+@onready var sfx: AudioStreamPlayer2D = $SFX
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		animated_sprite_2d.play("pickup")
+		if (!sfx.playing):
+			sfx.play()
 		await  animated_sprite_2d.animation_finished
 		queue_free()
